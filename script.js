c694e8f5577ff1575cb153e3c2f5a36e7dc91ed8
tanvibelsare <118069064+tanvibelsare@users.noreply.github.com>
2023-08-03 22:47:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,21 +9,25 @@ class Person {
 		 return this.name
 	 }
 	set age(age) {
-    this._age = age;
+    this.age = age;
   }
 }
 
 class Student extends Person {
-	
+	constructor(name,age){
+		super(name,age)
+	}
 	 study() {
-    console.log(`${this.name} is studying.`);
+    console.log(`${this.name} is studying`);
   }
 }
 
 class Teacher extends Person {
-	
+	constructor(name,age){
+		super(name,age)
+	}
 	 teach() {
-    console.log(`${this._name} is teaching.`);
+    console.log(`${this._name} is teaching`);
   }
 }
 
